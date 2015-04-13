841a745bcd54b5c003ce799576dce5029d6470ee
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-04-13 23:15:18 +0200

    Ajout d'une jQuery pour faire des barres de progression

--- app/scripts/controllers/skills.js
+++ app/scripts/controllers/skills.js
@@ -14,4 +14,24 @@ angular.module('myWebSiteApp')
       'AngularJS',
       'Karma'
     ];
+
+    /*======= Skillset *=======*/
+    angular.element(document).ready(function() {
+
+      angular.element('.level-bar-inner').css('width', '0');
+      angular.element(window).on('load', function() {
+        angular.element('.level-bar-inner').each(function() {
+          var itemWidth = angular.element(this).data('level');
+          angular.element(this).animate({
+            width: itemWidth
+          }, 800);
+        });
+      });
+
+      /* Bootstrap Tooltip for Skillset */
+      angular.element('.level-label').tooltip();
+      angular.element('.level-label2').tooltip();
+
+
+    });
   }]);
