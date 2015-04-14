d21f572645a7b1911f6f948e95ff337084aaa816
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-04-14 22:20:31 +0200

    work experience json

--- app/scripts/controllers/workexperience.js
+++ app/scripts/controllers/workexperience.js
@@ -8,10 +8,18 @@
  * Controller of the projetVitrineApp
  */
 angular.module('myWebSiteApp')
-  .controller('WorkexperienceCtrl', ['$scope', function ($scope) {
+  .controller('WorkexperienceCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $http.get('assets/json/workExperience.json')
+      .success(function(data) {
+        $scope.workExperience = data;
+      })
+      .error(function(error) {
+        console.log(error);
+      });
   }]);
