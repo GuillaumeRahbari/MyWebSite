3f43845e7c136d8d28b767480f4072f24dc03779
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-04-14 21:24:58 +0200

    ajout de languages json

--- app/scripts/controllers/languages.js
+++ app/scripts/controllers/languages.js
@@ -8,10 +8,18 @@
  * Controller of the myWebSiteApp
  */
 angular.module('myWebSiteApp')
-  .controller('LanguagesCtrl', ['$scope', function ($scope) {
+  .controller('LanguagesCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $http.get('assets/json/languages.json')
+      .success(function(data) {
+        $scope.languages = data;
+      })
+      .error(function(error) {
+        console.log(error);
+      });
   }]);
