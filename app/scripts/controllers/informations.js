9effa3945df8465556dd218b708f9b759dbe44cb
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-04-14 19:09:55 +0200

    json pour informations

--- app/scripts/controllers/informations.js
+++ app/scripts/controllers/informations.js
@@ -8,10 +8,19 @@
  * Controller of the myWebSiteApp
  */
 angular.module('myWebSiteApp')
-  .controller('InformationsCtrl', ['$scope', function ($scope) {
+  .controller('InformationsCtrl', ['$scope','$http', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $http.get('assets/json/informations.json')
+      .success(function(data) {
+        $scope.informations = data;
+      })
+      .error(function(error) {
+        console.log(error);
+      });
+
   }]);
