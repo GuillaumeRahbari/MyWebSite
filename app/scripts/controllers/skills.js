96f33b7e02743b79a06105616aba7b6b35ce173e
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-04-14 21:53:13 +0200

    skills json

--- app/scripts/controllers/skills.js
+++ app/scripts/controllers/skills.js
@@ -8,12 +8,20 @@
  * Controller of the projetVitrineApp
  */
 angular.module('myWebSiteApp')
-  .controller('SkillsCtrl', ['$scope', function ($scope) {
+  .controller('SkillsCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $http.get('assets/json/skills.json')
+      .success(function(data) {
+        $scope.skills = data;
+      })
+      .error(function(error) {
+        console.log(error);
+      });
 
     /*======= Skillset *=======*/
     angular.element(document).ready(function() {
