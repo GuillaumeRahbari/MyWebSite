d3712a25ae999f60b63e7f4c0a7bc626de478b83
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-04-15 18:18:13 +0200

    suppression du progress bar de skills.js

--- app/scripts/controllers/skills.js
+++ app/scripts/controllers/skills.js
@@ -22,30 +22,4 @@ angular.module('myWebSiteApp')
       .error(function(error) {
         console.log(error);
       });
-
-    /*======= Skillset *=======*/
-    angular.element(document).ready(function() {
-
-      angular.element('.level-bar-inner').css('width', '0');
-
-      angular.element(window).on('load', function() {
-
-        angular.element('.level-bar-inner').each(function() {
-
-          var itemWidth = angular.element(this).data('level');
-
-          angular.element(this).animate({
-            width: itemWidth
-          }, 800);
-
-        });
-
-      });
-
-      /* Bootstrap Tooltip for Skillset */
-      angular.element('.level-label').tooltip();
-      angular.element('.level-label2').tooltip();
-
-
-    });
   }]);
